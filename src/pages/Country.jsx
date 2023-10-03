c9d5023abae545701b97740304d789fd2094ca47
olakad13 <olakad13@gmail.com>
2023-10-03 15:47:45 +0100

    .

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -11,7 +11,7 @@ export default function Home() {
 
     const { name } = useParams();
     const { isDark, countries, unchangedData } = useContext(Context);
-    const navigate = useNavigate();
+    
     
   
 
